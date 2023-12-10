4f24d45db250e3cadee98e9aae9fcc1dfa7b2f5c
Joyo Nurdiansyah <nurdiansyahjoyo@gmail.com>
2023-12-10 20:00:49 +0700

    rev banner logo dan text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,8 @@
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                     <span className="tagline">Welcome to my Portfolio</span>
                     <h1>{`Hi! I'm Joyo`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                    <p>Welcome to my portfolio. I am a passionate Junior Web Developer with a focus on web development using PHP and JavaScript. With a strong background in both programming languages, I am suited to create innovative, responsive and user-friendly web solutions.</p>                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                    <p>Welcome to my portfolio. I am a passionate Junior Web Developer with a focus on web development using PHP and JavaScript. With a strong background in both programming languages, I am suited to create innovative, responsive and user-friendly web solutions.</p>                  
+                    <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
                 </div>}
                 </TrackVisibility>
             </Col>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
     import { useState, useEffect } from "react"
     import { Navbar, Container, Nav } from "react-bootstrap"
-    import logo from '../assets/img/logo.svg';
+    import logo from '../assets/img/logo-joy.png';
     import navIcon1 from '../assets/img/nav-icon1.svg';
     import navIcon2 from '../assets/img/nav-icon2.svg';
     import navIcon3 from '../assets/img/nav-icon3.svg';
@@ -31,7 +31,7 @@
                 <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
                     <Container>
                     <Navbar.Brand href="/">
-                        <img src={logo} alt="Logo" />
+                        <img src={logo} alt="Logo" style={{ maxWidth: '60px' }} />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav">
                         <span className="navbar-toggler-icon"></span>
@@ -44,11 +44,13 @@
                         </Nav>
                         <span className="navbar-text">
                         <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/joyo-nurdiansyah/"><img src={navIcon1} alt="" /></a>
-                            <a href="https://www.instagram.com/joyonurdiansyah/"><img src={navIcon2} alt="" /></a>
-                            <a href="#"><img src={navIcon3} alt="" /></a>
+                            <a href="https://www.linkedin.com/in/joyo-nurdiansyah/"><img src={navIcon1} alt="linkedin" /></a>
+                            <a href="https://www.facebook.com/joyo.nurdiansyah.7"><img src={navIcon2} alt="facebook" /></a>
+                            <a href="https://www.instagram.com/joyonurdiansyah/"><img src={navIcon3} alt="instagram" /></a>
                         </div>
-                            <button className="vvd"><span>Let’s Connect</span></button>
+                        {/* <a href="https://wa.me/081316133984?text=Halo%20Bang%20Joy">
+                            <button class="vvd"><span>Let’s Connect</span></button>
+                        </a> */}
                         </span>
                     </Navbar.Collapse>
                     </Container>
